a190bb807e3b02d89aa453204ea6b4a133fc40df
sjxiang <1535484943@qq.com>
2023-01-15 00:31:10 +0800

    修复bug

--- net/collect/fetch_test.go
+++ net/collect/fetch_test.go
@@ -1,3 +1,17 @@
 package collect
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestFetch(t *testing.T) {
+	url := ""
+
+	reply, err := Query(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*reply)
+}
